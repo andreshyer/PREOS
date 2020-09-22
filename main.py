d1c14f7d6bba70d7cf03b3bfd201920bc6d06fa1
Andres Hyer <hyerap@vcu.edu>
2020-09-22 17:02:31 -0400

    update csv backbone files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class PendRob:
 
-    def __init__(self, compound):
+    def __init__(self):
 
         """
         Goal is to solve for Z and V in PREOS
@@ -20,38 +20,55 @@ class PendRob:
         B = bP / RT
 
         Z = PV / RT
-
-        :param compound: compound to calculate V and Z for
         """
 
-        self.compound = compound
-
-        self.ab_data = pd.read_csv('preso_ab.csv')
-        self.ab_data = self.ab_data.rename(columns={'a (Pa*m^6*mol^-2)': 'a',
-                                                    'b [(m^3/mol)*10^5]': 'b'})
-
-        if self.compound not in list(self.ab_data['compound']):
-            compounds = ', '.join(list(self.ab_data['compound']))
-            raise ValueError(
-                f"Compound {self.compound} was not found in spreadsheet.\n"
-                f"Available compounds are: {compounds}\n"
-                "Please check spelling or add compound to spreadsheet."
-                )
+        # Gather compounds, make sure they are correct...
+        # self.compounds = {'oxygen': 20, 'nitrogen': 60, 'water': 20}
+        self.compounds = {'methane': 1}
+        self.a_data = dict(zip(self.compounds.keys(), [np.nan] * len(self.compounds)))
+        self.b_data = dict(zip(self.compounds.keys(), [np.nan] * len(self.compounds)))
 
         self.R = 8.314  # In Pa * m^3 * K^-1 * mol^-1
-        self.a, self.b = self.__fetch_a_b__()
 
-    def __fetch_a_b__(self):
-        ab_data = self.ab_data[self.ab_data['compound'] == self.compound]
-        ab_data = ab_data.to_dict('records')[0]
-        a = ab_data['a']
-        b = ab_data['b']
+        self.raw_Tc_Pc_w_data = pd.read_csv('preso_ab.csv')
+        self.raw_Tc_Pc_w_data = self.raw_Tc_Pc_w_data.rename(columns={'Tc (K)': 'Tc', 'Pc (MPa)': 'Pc'})
+
+        self.a_mix = np.nan
+        self.b_mix = np.nan
+
+    def __calculate_a_b__(self, temperture, compound):
+        compound_data = self.raw_Tc_Pc_w_data[self.raw_Tc_Pc_w_data['compound'] == compound]
+        compound_data = compound_data.to_dict('records')[0]
+        Tc = compound_data['Tc']
+        Pc = compound_data['Pc'] * 10**6  # Convert MPa to Pa
+        w = compound_data['w']
+
+        k = 0.37464 + 1.54226*w - 0.26992*(w**2)
+        alpha = (1 + k*(1 - (temperture/Tc)**(1/2)))**2
+
+        a = 0.45724 * alpha * (self.R*Tc)**2/Pc
+        b = 0.07780 * self.R*Tc/Pc
+
         return a, b
 
-    def calculate_z(self, pressure, temperature):
+    def __calculate_a_b_mix__(self):
+
+        k = 0
+        a_mix = 0
+        b_mix = 0
+
+        for compound, y1 in self.compounds.items():
+            b_mix += y1 * self.b_data[compound]
+            for sub_compound, y2 in self.compounds.items():
+                a1 = self.a_data[compound]
+                a2 = self.a_data[sub_compound]
+                a_mix += (a1*a2)**(1/2)
+
+        return a_mix, b_mix
+
+    def calculate_Z(self, pressure, temperature):
 
         """
-
         :param pressure: absolute pressure of gas in bar
         :param temperature: temperture of gas in C
         :return: z constant for gas at conditions given
@@ -63,15 +80,15 @@ class PendRob:
         if temperature < 0:
             raise ValueError("Temperature cannot be less than 0K")
 
+        for compound in self.compounds:
+            self.a_data[compound], self.b_data[compound] = self.__calculate_a_b__(temperature, compound)
+
+        self.a_mix, self.b_mix = self.__calculate_a_b_mix__()
+
         A, B = self.__calculate_A_B__(pressure, temperature)
         u, w, v = self.__calculate_u_w_v__(A, B)
-        z = self.__calculate_z__(u, w, v)
+        z = self.__calculate_z_roots__(u, w, v)
         return z
-
-    def calculate_V(self, pressure, temperature):
-        z = self.calculate_z(pressure, temperature)
-        V = z * pressure * temperature / self.R
-        return V
 
     @staticmethod
     def __convert_variables__(pressure, temperature):
@@ -80,8 +97,8 @@ class PendRob:
         return pressure, temperature
 
     def __calculate_A_B__(self, P, T):
-        A = (self.a * P) / (self.R * T)**2
-        B = (self.b * P) / (self.R * T)
+        A = (self.a_mix * P) / ((self.R * T) ** 2)
+        B = (self.b_mix * P) / (self.R * T)
         return A, B
 
     @staticmethod
@@ -92,13 +109,34 @@ class PendRob:
         return u, w, v
 
     @staticmethod
-    def __calculate_z__(u, w, v):
+    def __calculate_z_roots__(u, w, v):
         roots = np.roots([1, u, v, w])
         roots = roots[np.isreal(roots)]
         roots = np.real(roots)
-        return max(roots)
+        if len(roots) == 3:
+            roots = {'gas': max(roots), 'liquid': min(roots)}
+        elif len(roots) == 1:
+            roots = {'single state': roots[0]}
+        else:
+            raise Exception("Something broke, there should not be only two real roots")
+        return roots
+
+    def calculate_V(self, pressure, temperature):
+
+        """
+        :param pressure: absolute pressure of gas in bar
+        :param temperature: temperture of gas in C
+        :return: molar volume in m^3/mol for gas at conditions given
+        """
+
+        all_z = self.calculate_Z(pressure, temperature)
+        pressure, temperature = self.__convert_variables__(pressure, temperature)
+        V = {}
+        for state, z in all_z.items():
+            V[state] = z * self.R * temperature / pressure
+        return V
 
 
-oxygen = PendRob(compound='helium')
-value = oxygen.calculate_V(pressure=0.05, temperature=300)
+calculator = PendRob()
+value = calculator.calculate_V(pressure=0.1, temperature=300)
 print(value)
